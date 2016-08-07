b5ed0e620a741638e718042ef80d2d7a7558e012
Edwin Marcial <edwin_ma@yahoo.com>
2016-08-07 17:45:05 -0400

    results update

--- server.js
+++ server.js
@@ -243,7 +243,7 @@ function createSchedule2016()
         createEvent("July 17", "12 PM", "South Gwinett", "Dragons", "L 5-2", "", ""),
         createEvent("July 24", "", "", "No Game", "", "BYE", ""),
         createEvent("July 31", "4 PM", "Druid Hills Middle", "Raw Dawgs", "W 9-6", "", ""),
-        createEvent("Aug 7", "12 PM", "Druid Hills Middle", "@Titans", "", "", ""),
+        createEvent("Aug 7", "12 PM", "Druid Hills Middle", "@Titans", "L 6-5", "", ""),
         createEvent("Aug 14", "12 PM", "Druid Hills Middle", "@Cobb Red Sox", "", "", ""),
         createEvent("Aug 21", "12 PM", "Druid Hills Middle", "@Bandits", "", "", ""),
         createEvent("Aug 28", "12 PM", "Druid Hills Middle", "Dragons (18+)", "", "", ""),
