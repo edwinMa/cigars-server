1ba23b3c699a90e633e9a5adc5c43e0ad7ac0b16
Edwin Marcial <edwin_ma@yahoo.com>
2017-01-29 13:16:48 -0500

    fixed bug if run on Sunday

--- weather.js
+++ weather.js
@@ -64,32 +64,47 @@ Weather.prototype = {
             else if (res.statusCode !== 200) {
                 console.log('Status:', res.statusCode);
             } 
-            else {
-                debug (results);
-                debug (results.forecast);
+            else {                
                 var days = results.forecast.simpleforecast.forecastday;
 
                 var numDays = days.length;
+                debug ("num days returned: " + numDays);
+
                 for (j=0; j< numDays; j++)
                 {
                     debug (days[j].date.weekday);
                     if (days[j].date.weekday == "Sunday")
                     {
+                        debug ("creating new Sunday's forecast " );
+                        debug ("forecast array size " + WeekendForecast.length);
+
                         WeekendForecast [Sunday] = new DayForecast (days[j].high.fahrenheit, days[j].conditions, 
                                 (days[j].date.monthname + " " + days[j].date.day), days[j].icon_url);
+                        debug ("forecast array size " + WeekendForecast.length);
+
                     }
                     else if (days[j].date.weekday == "Saturday")
                     {
+                        debug ("creating new Saturdays's forecast " );
                         WeekendForecast [Saturday] = new DayForecast (days[j].high.fahrenheit, days[j].conditions, 
                                 (days[j].date.monthname + " " + days[j].date.day), days[j].icon_url);
                     }
                 }
                 
-                debug ("Saturdays's forecast " + WeekendForecast [Saturday].conditions + " " + WeekendForecast [Saturday].hiTemp);
-                debug ("Sunday's forecast " + WeekendForecast [Sunday].conditions + " " + WeekendForecast [Sunday].hiTemp);
+                if (WeekendForecast [Saturday] != null)
+                {
+                    debug ("Saturdays's forecast " + WeekendForecast [Saturday].conditions + " " + WeekendForecast [Saturday].hiTemp);
+                }
 
-                // invoke callback function
+
+                if (WeekendForecast [Sunday] != null)
+                {
+                    debug ("Sunday's forecast " + WeekendForecast [Sunday].conditions + " " + WeekendForecast [Sunday].hiTemp);
+                }
+
+                // invoke callback function                 
                 if (typeof callback === "function") {
+                    debug ("returning callback, forecast array size " + WeekendForecast.length);
                     callback (WeekendForecast);
                 }
                 
