968cab80dd5b496971f1c5087afe880925399a5e
Edwin Marcial <edwin_ma@yahoo.com>
2017-03-27 16:49:43 -0400

    update

--- roster.js
+++ roster.js
@@ -82,8 +82,8 @@ class Roster
             new Player ("Chad Lambert", "Lambert", "23", "SS/2B/P", "", "R", "R", "", ""),
             // new Player ("Jamie Houston", "Houston", "39", "IF/OF", "", "R", "R", "", ""),
             new Player ("Adrian Prieto", "Suarez", "4", "IF/P/OF", "", "R", "R", "", ""),
-            new Player ("Jonathan Roberts", "Roberts", "X", "OF/P", "", "R", "R", "", ""),
-            new Player ("Thomas Smith", "Smith", "X", "IF", "", "R", "R", "", ""),
+            new Player ("Jonathan Roberts", "Roberts", "1", "OF/P", "", "R", "R", "", ""),
+            new Player ("Thomas Smith", "Smith", "32", "2B/SS", "", "R", "R", "", ""),
             new Player ("James Little", "Little", "3", "C", "Farmers Branch, TX", "R", "R", "", "Shades of Gray - Robert Earl Keen")
             ];
         
