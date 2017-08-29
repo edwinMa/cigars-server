c2db1bd2bc457d15b1314a506d71d6a216d9372c
Edwin Marcial <edwin_ma@yahoo.com>
2017-08-29 17:37:32 -0400

    update

--- schedule.js
+++ schedule.js
@@ -25,11 +25,11 @@ var games2017 = [
         new Event ("August 13", "1030 AM", "Druid Hills Middle", "Cherokees", "W 4-3", "GC: Ryan Flemming, Sean Lennox", ""),
         new Event ("August 13", "1 PM", "Druid Hills Middle", "Cherokees", "W 6-3", "GC: Phil Lucas", ""),
         new Event ("August 20", "4 PM", "Druid Hills Middle", "@Barracudas", "W 6-1", "GC: Phil Lucas", ""),
-        new Event ("August 27", "10 AM", "Druid Hills Middle", "Dragons", "", "", ""),
+        new Event ("August 27", "10 AM", "Druid Hills Middle", "Dragons", "L 4-1", "", ""),
         new Event ("September 3", "", "", "No Game", "", "Labor Day", ""),
-        new Event ("September 9", "12 PM", "TBD", "TBD", "Playoffs Game 1", "", ""),
-        new Event ("September 9", "4 PM", "TBD", "TBD", "Playoffs Game 2", "", ""),
-        new Event ("September 10", "12 PM", "TBD", "TBD", "Playoffs Game 3 (if necessary)", "", "")
+        new Event ("September 9", "1215 PM", "@Outlaws", "South Gwinett HS", "Playoffs Game 1", "", ""),
+        new Event ("September 9", "3 PM", "Outlaws", "South Gwinett HS", "Playoffs Game 2", "", ""),
+        new Event ("September 10", "12 PM", "Outlaw", "South Gwinnnet HS", "Playoffs Game 3 (if necessary)", "", "")
 
     ];
 
