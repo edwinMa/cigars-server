fc078e9752f04118ac0a39336887bead5e6f72fe
Edwin Marcial <edwin_ma@yahoo.com>
2018-09-10 23:26:24 -0400

    update

--- schedule.js
+++ schedule.js
@@ -34,7 +34,7 @@ var games2018 = [
         new Event ("September 2", "", "", "No Game", "", "Labor Day Weekend", ""),
         new Event ("September 8", "4 PM", "Druid Hills Middle", "@Cherokees", "W 9-4", "Playoff Game 1", ""),
         new Event ("September 8", "1030 AM", "Druid Hills Middle", "Cherokees", "L 6-2", "Playoff Game 2", ""),
-        new Event ("September 9", "130", "Druid Hills Middle", "@Cherokees", "L 9-2", "Playoff Game 3", "")
+        new Event ("September 9", "130 PM", "Druid Hills Middle", "@Cherokees", "L 9-2", "Playoff Game 3", "")
 
     ];
 
