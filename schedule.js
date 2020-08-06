a535d893ae28044fc9293878df0aa1b110478379
Edwin Marcial <edwin019@gmail.com>
2020-08-06 18:44:34 -0400

    update

--- schedule.js
+++ schedule.js
@@ -5,7 +5,7 @@ var debug = require ('./debug');
 var games2020 = [
         new Event ("Sun July 19", "1:15 PM", "Osborne", "Muckdogs", "W 8-0", "", ""),
         new Event ("Sun July 26", "4:30 PM", "South Cobb", "@Tainos", "W 2-0", "", ""),
-        new Event ("Sun Aug 2", "12 PM", "Big Shanty", "Cherokees", "", "", ""),
+        new Event ("Sun Aug 2", "12 PM", "Big Shanty", "Cherokees", "L 5-2", "", ""),
         new Event ("Sun Aug 9", "", "", "@Cobb Angels", "", "", "")
 
 
