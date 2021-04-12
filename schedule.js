9c257002a19afa9b52d41a4d47982d4151056472
Edwin Marcial <edwin019@gmail.com>
2021-04-11 20:58:49 -0400

    2021 udpate

--- schedule.js
+++ schedule.js
@@ -3,8 +3,10 @@ var debug = require ('./debug');
 
 var games2021 = [
         // last column is a url link that links back from the date - first column
-        new Event ("Sun Nov 22", "1230 PM", "Lake Point", "Tainos", "L 11-2", "Playoff Finals - Game 3", ""),
-        new Event ("Sun April 11", "4:00 PM", "South Cobb", "@Cherokees", "", "", "")
+        new Event ("Sun Nov 22", "1230 PM", "Lake Point", "Tainos", "L 11-2", "Championship Finals - Game 3", ""),
+        new Event ("Sun April 11", "4:00 PM", "Lakeoint #11", "@Cherokees", "W 10-8", "", ""),
+        new Event ("Sun April 18", "4:00 PM", "TBD", "Cobb Angels", "", "", "")
+
         
 ];
 
