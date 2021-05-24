f4381570bbe6f77a7de04d2ebc7cfa9c484b08cd
Edwin Marcial <edwin019@gmail.com>
2021-05-23 20:28:58 -0400

    2021 udpate

--- schedule.js
+++ schedule.js
@@ -8,7 +8,7 @@ var games2021 = [
         new Event ("Sun April 18", "4:00 PM", "South Cobb", "Cobb Angels", "W 17-15", "", ""),
         new Event ("Sun May 2", "3:00 PM", "Hunter Park", "Tainos", "L 5-4", "", ""),
         new Event ("Sun May 16", "12:00 PM", "Shamrock", "Tornados", "W 11-3", "", ""),
-        new Event ("Sun May 23", "12:00 PM", "Shamrock", "@Cobb Angels", "", "", ""),
+        new Event ("Sun May 23", "12:00 PM", "Shamrock", "@Cobb Angels", "W 14-2", "", ""),
         new Event ("Sun June 6", "12:00 PM", "Shamrock", "Dragons", "", "", "")
 
         
