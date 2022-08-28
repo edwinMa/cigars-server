bd98f3936f2f4258ef33d63b9818ca0b447d13b8
Edwin Marcial <edwin019@gmail.com>
2022-08-28 15:44:37 -0400

    scores update

--- schedule.js
+++ schedule.js
@@ -24,9 +24,10 @@ var games2022 = [
         new Event ("Sat Aug 20", "1:15 PM", "Lake Point", "Cobb Angels", "W 14-9", "", "Playoff Round 1"),
         new Event ("Sat Aug 20", "4:00 PM", "Lake Point", "@Cobb Angels", "W 12-4", "", "Playoff Round 1"),
         new Event ("Sat Aug 27", "1 PM", "South Gwinett", "Outlaws", "W 4-1", "", "Playoff Semi-Final"),
-        new Event ("Sat Aug 27", "4 PM", "South Gwinett", "Outlaws", "W 6-2", "", "Playoff Semi-Final"),
-        new Event ("Sat Sep 10", "TBD", "TBD", "TBD", "", "", "Playoff Championship")
-
+        new Event ("Sat Aug 27", "4 PM", "South Gwinett", "@Outlaws", "W 6-2", "", "Playoff Semi-Final"),
+        new Event ("Sat Sep 10", "TBD", "TBD", "@Dragons", "", "", "Playoff Championship"),
+        new Event ("Sat Sep 10", "TBD", "TBD", "Dragons", "", "", "Playoff Championship"),
+        new Event ("Sat Sep 11", "TBD", "TBD", "@Dragons", "", "", "Playoff Championship")
 
 
 ];
